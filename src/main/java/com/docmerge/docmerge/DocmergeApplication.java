9e3e2a0c23f9d724aee0a3967653a7919ca4b13c
SudhanshuMishraJ <130904198+SudhanshuMishraJ@users.noreply.github.com>
2025-03-26 15:51:53 +0530

    First commit

---
+++ src/main/java/com/docmerge/docmerge/DocmergeApplication.java
@@ -0,0 +1,13 @@
+package com.docmerge.docmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocmergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docmerge/docmerge/DocmergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docmerge.docmerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
